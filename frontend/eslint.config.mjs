6fe41ae76f766d9aeaf7925704565113206738da
devghod <gbtm.workspace@gmail.com>
2025-06-13 14:27:34 +0800

    Eslint update

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
-      'beforeStatementContinuationChars': 'off',
+      beforeStatementContinuationChars: 'off',
     },
   },
 ];
