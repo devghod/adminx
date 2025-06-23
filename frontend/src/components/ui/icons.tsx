d5f6b082a6dab71afc16ebb7b86c408822815a0f
Devghod <gbtm.workspace@gmail.com>
2025-06-23 12:38:44 +0800

    Add female and male icon

--- frontend/src/components/ui/icons.tsx
+++ frontend/src/components/ui/icons.tsx
@@ -726,6 +726,40 @@ function EmployeeIcon(props: React.SVGProps<SVGSVGElement>) {
   );
 }
 
+function UserMaleIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='22'
+      height='24'
+      viewBox='0 0 22 24'
+    >
+      <path
+        fill='currentColor'
+        d='M14.145 16.629a24 24 0 0 1-.052-2.525l-.001.037a4.85 4.85 0 0 0 1.333-2.868l.002-.021c.339-.028.874-.358 1.03-1.666a1.22 1.22 0 0 0-.455-1.218l-.003-.002c.552-1.66 1.698-6.796-2.121-7.326C13.485.35 12.479 0 11.171 0c-5.233.096-5.864 3.951-4.72 8.366a1.22 1.22 0 0 0-.455 1.229l-.001-.008c.16 1.306.691 1.638 1.03 1.666a4.86 4.86 0 0 0 1.374 2.888a25 25 0 0 1-.058 2.569l.005-.081C7.308 19.413.32 18.631 0 24h22.458c-.322-5.369-7.278-4.587-8.314-7.371z'
+      />
+    </svg>
+  );
+}
+
+function UserFemaleIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='22'
+      height='24'
+      viewBox='0 0 22 24'
+    >
+      <path
+        fill='currentColor'
+        d='M14.041 16.683a15 15 0 0 1-.035-.72c2.549-.261 4.338-.872 4.338-1.585c-.007 0-.006-.03-.006-.041C16.432 12.619 19.99.417 13.367.663a3.34 3.34 0 0 0-2.196-.664h.008C2.208.677 6.175 12.202 4.13 14.377h-.004c.008.698 1.736 1.298 4.211 1.566c-.007.17-.022.381-.054.734C7.256 19.447.321 18.671.001 24h22.294c-.319-5.33-7.225-4.554-8.253-7.317z'
+      />
+    </svg>
+  );
+}
+
 export {
   AdminIcon,
   ManagerIcon,
@@ -754,6 +788,8 @@ export {
   LogOutIcon,
   LineChartIcon,
   UserIcon,
+  UserMaleIcon,
+  UserFemaleIcon,
   UsersIcon,
   UsersNetworkIcon,
   PackageIcon,
